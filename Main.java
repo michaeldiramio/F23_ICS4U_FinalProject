fba1942fd055d9ca57f91325f937b72656467b7a
LiamDoyle24 <145358123+LiamDoyle24@users.noreply.github.com>
2024-01-10 14:18:59 +0000

    Adding Circle Game

--- Main.java
+++ Main.java
@@ -10,12 +10,19 @@ public class Main {
     ArrayList<Player> players = new ArrayList<Player>();
     Random rnd = new Random();
 		dc.setOrigin(DConsole.ORIGIN_CENTER);
+    
     players.add(new Player(dc, 87, 65, 83, 68, 69));
     players.add(new Player(dc, 38, 37, 40, 39, 13));
-		
+
+
+    CircleGame c = new CircleGame (dc, players.get(0), players.get(1));
 		dc.fillEllipse(225, 150, 50, 50);
 		dc.redraw();
-		
+
+    c.run();
+    
+
+    
 	}
 	
 }
