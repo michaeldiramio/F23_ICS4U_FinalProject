5f6a8cde2f29a5ddf5f699632aca17de3d21e483
ATRUSEVIC <152985101+ATRUSEVIC@users.noreply.github.com>
2024-01-10 13:35:44 +0000

    started RPS

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import DLibX.*;
 public class Main {
 
 	public static void main(String[] args) {
-		DConsole dc = new DConsole(450, 300);
+		DConsole dc = new DConsole(900, 600);
 		dc.setOrigin(DConsole.ORIGIN_CENTER);
 		
 		dc.fillEllipse(225, 150, 50, 50);
