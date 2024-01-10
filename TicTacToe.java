960f533a76cd4edd37b64ab0bed4ec72a6c3c78b
LiamDoyle24 <145358123+LiamDoyle24@users.noreply.github.com>
2024-01-10 14:51:31 +0000

    Created new file

---
+++ TicTacToe.java
@@ -0,0 +1,27 @@
+import DLibX.DConsole;
+import java.awt.Color;
+import java.awt.geom.AffineTransform;
+import java.util.*;
+
+public class TicTacToe extends baseGame {
+
+  DConsole dc;
+  private Player player1;
+  private Player player2;
+
+  private int[][] gameBoard = new int[3][3];
+  private int pointerX = 0;
+  private int pointerY = 0;
+  
+  
+
+  
+  public void initialize (Player p1, Player p2){
+    
+  }
+  
+  public void run(){
+    
+  }
+  
+}
