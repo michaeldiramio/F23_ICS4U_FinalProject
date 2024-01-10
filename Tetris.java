f03cf17e9c3607ad7cc8e494a9dbafb2f50e2ce0
cieszkoa <152986376+cieszkoa@users.noreply.github.com>
2024-01-10 14:22:14 +0000

    still working on

---
+++ Tetris.java
@@ -0,0 +1,32 @@
+import DLibX.*;
+
+public class Tetris extends basegame {
+	
+}
+
+public tetris (DConsole dc, )
+
+public class run(){
+	
+}
+
+		color[] random = new color[6];
+		
+		Red = new color(255,0,0);
+		Green = new color(0,255,0);
+		Blue = new color(0,0,255);
+		Yellow = new color(209, 203, 13);
+		White = new color(255,255,255);
+		Black = new color(0,0,0);
+		
+		color[0] = Red;
+		color[1] = Green;
+		color[2] = Blue;
+		color[3] = Yellow;
+		color[4] = White;
+		color[5] = Black;
+	
+		// player
+		dc.fillRect(150,150,10,10);
+		
+		dc.redraw();
