663f62f9712ec22346f8bbbf097026049718edeb
LiamDoyle24 <145358123+LiamDoyle24@users.noreply.github.com>
2024-01-11 19:03:20 +0000

    added gridSquare object

---
+++ ticTacToe/ticTacToe.java
@@ -0,0 +1,68 @@
+import DLibX.DConsole;
+import java.awt.Color;
+import java.awt.geom.AffineTransform;
+import java.util.*;
+
+public class ticTacToe extends baseGame {
+
+  DConsole dc;
+  Random randGen;
+  private Player player1;
+  private Player player2;
+  private int activePlayer = 0;
+  private boolean gameOver = false;
+
+  private gridSquare[][] gameBoard = new gridSquare[3][3];
+  
+  public ticTacToe(DConsole dc, Random rng, Player p1, Player p2){
+    this.dc = dc;
+    this.randGen = rng;
+    this.player1 = p1;
+    this.player2 = p2;
+  }
+  
+  public void initialize (){
+    this.activePlayer = this.randGen.nextInt(2);
+    int xPosition = 200;
+    int yPosition = 200;
+    for(int i = 0; i < gameBoard.length; i++){
+      for(int j = 0; j < gameBoard[i].length; j++){
+        yPosition += (j * yPosition);
+        gameBoard[i][j] = new gridSquare(dc, xPosition, yPosition, 200);
+      }
+      xPosition += (i * xPosition);
+    }
+  }
+  
+  public void run(){
+    this.drawBackground();
+
+    while(!gameOver){
+     
+      int mouseX = dc.getMouseXPosition();
+      int mouseY = dc.getMouseYPosition();
+
+
+      
+
+
+      
+      
+    }
+    
+    
+    
+    
+    
+    dc.redraw();
+  }
+
+  public void checkForWinner(){
+    
+  }
+
+  public void drawBackground(){
+    
+  }
+  
+}
