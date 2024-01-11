951f47cba8607b6366d6774899eef95e6b2ff794
moxonm <152993140+moxonm@users.noreply.github.com>
2024-01-11 13:32:05 +0000

    started game needed to pull

---
+++ ClickGame.java
@@ -0,0 +1,27 @@
+import DLibX.DConsole;
+import java.awt.Color;
+import java.awt.geom.AffineTransform;
+import java.util.*;
+
+public class ClickGame extends baseGame {
+
+  DConsole dc;
+  private Player player1;
+  private Player player2;
+  
+  public ClickGame(DConsole dc, Player p1, Player p2){
+    player1 = p1;
+    player2 = p2;
+    this.dc = dc;
+  }
+  
+  public void initialize (){
+    
+  }
+  
+  public void run(){
+    dc.drawEllipse(50,50,50,50);
+    dc.redraw();
+  }
+  
+}
