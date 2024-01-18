ac34767e81ce38ac62febe35ede857abf94c67b3
Aidoggy95 <152986908+Aidoggy95@users.noreply.github.com>
2024-01-18 13:50:39 +0000

    Fixed GuessingGame Errors

--- GuessingGame.java
+++ GuessingGame.java
@@ -7,4 +7,16 @@ public class GuessingGame extends baseGame {
   DConsole dc;
   private Player p1;
   private Player p2;
+
+  public GuessingGame(DConsole dc, Player p1, Player p2){
+    
+  }
+  
+  public void run(){
+    
+  }
+
+  public void initialize(){
+    
+  }
 }
--- Main.java
+++ Main.java
@@ -35,7 +35,7 @@ public class Main {
     this.games.add(new ticTacToe (this.dc, this.rnd, this.players.get(0), this.players.get(1))); 
     this.games.add(new DontGrabOrbGame (this.dc, this.players.get(0), this.players.get(1)));
     this.games.add(new TugOfWar (this.dc, this.players.get(0), this.players.get(1)));
-    this.games.add(new Guessing (this.dc, this.players.get(0), this.players.get(1)));
+    this.games.add(new GuessingGame (this.dc, this.players.get(0), this.players.get(1)));
     
     //run game loop
     System.out.println("Game initialized -- Running main loop");
