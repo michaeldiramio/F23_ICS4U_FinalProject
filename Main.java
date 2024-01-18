972016677b169ba28c773a677211f3575bf6b248
ATRUSEVIC <152985101+ATRUSEVIC@users.noreply.github.com>
2024-01-18 13:48:58 +0000

    Completed SpeedWord game

--- Main.java
+++ Main.java
@@ -33,6 +33,7 @@ public class Main {
     this.games.add(new RPS (this.dc, this.players.get(0), this.players.get(1)));
     this.games.add(new ticTacToe (this.dc, this.rnd, this.players.get(0), this.players.get(1))); 
     this.games.add(new DontGrabOrbGame (this.dc, this.players.get(0), this.players.get(1)));
+    this.games.add(new SpeedWord (this.dc, this.players.get(0), this.players.get(1), this.rnd));
     
     //run game loop
     System.out.println("Game initialized -- Running main loop");
