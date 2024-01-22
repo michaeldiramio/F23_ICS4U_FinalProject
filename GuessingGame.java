767b3b2047874bd554071e174635e67dce7b9c95
Aidoggy95 <152986908+Aidoggy95@users.noreply.github.com>
2024-01-22 13:29:48 +0000

    Guessing Game

--- GuessingGame.java
+++ GuessingGame.java
@@ -9,14 +9,41 @@ public class GuessingGame extends baseGame {
   private Player p2;
 
   public GuessingGame(DConsole dc, Player p1, Player p2){
-    
+     this.p1 = p1;
+     this.p2 = p2;
+     this.dc = dc;
   }
   
   public void run(){
-    
+    while (true){
+      this.dc.clear();
+
+      Scanner userIn = new Scanner(System.in);
+
+      System.out.println("Player 2 look away while Player 1 picks the key");
+
+      String Key = userIn.nextLine();
+
+      System.out.println("Player 1, inform Player 2 that it is his turn to guess");
+
+      String Guess = userIn.nextLine();
+
+      if(Key.equals(Guess)){
+        System.out.println("Player 2 Wins");
+        super.winner = 2;
+        break;
+      }
+
+      if(!Key.equals(Guess)){
+        System.out.println("Player 1 Wins");
+        super.winner = 1;
+        break;
+    }
+  }
   }
 
-  public void initialize(){
+  public void initialize() {
     
+    this.run();
   }
 }
